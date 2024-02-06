4dd998d58d192becf6e5e7594ef4180a3913d375
Asim Mutlu <asimmutlu6134@gmail.com>
2024-02-06 17:55:32 +0300

    Add project files.

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,8 +1,61 @@
 ﻿//Degiskenler();
 
+using Business.Concrete;
+using Entities.Concrete;
+
 Vatandas vatandas = new Vatandas();
 
+SelamVer("Asım");
+Topla(3,5);
+
+// Diziler /Arrays
+
+string[] ogrenciler = new string[3];
+ogrenciler[0] = "Engin";
+ogrenciler[1] = "Kerem";
+ogrenciler[2] = "Berkay";
+
+for (int i = 0; i < ogrenciler.Length; i++)
+{
+    Console.WriteLine(ogrenciler[i]);
+}
+
+string[] sehirler = new[] {"Ankara","İstanbul","İzmir"};
+string[] sehirler2 = new[] {"Bursa","Antalya","Diyarbakır"};
+sehirler2 = sehirler;
+sehirler[0] = "Adana";
+Console.WriteLine(sehirler2[0]);
+
+foreach (var sehir in sehirler)
+    Console.WriteLine(sehir);
+
+
+List<string> yeniSehirler = new List<string>{"Ankara","İstanbul","İzmir"};
+yeniSehirler.Add("Trabzon");
+
+foreach (var sehir in yeniSehirler)
+    Console.WriteLine(sehir);
+
+
+
+Person person= new Person() {FirstName="ASIM",LastName="MUTLU",DateOfBirthYear=2001,NationalIdentity=1234};
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
+
 Console.ReadLine();
+
+static void SelamVer(string isim="isimsiz")
+{
+    Console.WriteLine("Merhaba "+isim);
+}
+
+static int Topla(int sayi1=5,int sayi2=10)
+{
+    int sonuc = sayi1 + sayi2;
+    Console.WriteLine("Toplam : "+sonuc);
+    return sonuc;
+}
 
 static void Degiskenler()
 {
@@ -11,9 +64,9 @@ static void Degiskenler()
     int sayi = 100;
     bool girisYapmisMi = false;
 
-    string ad = "Engin";
-    string soyad = "Demiroğ";
-    int dogumYili = 1985;
+    string ad = "Asım";
+    string soyad = "Mutlu";
+    int dogumYili = 2001;
     long tcNo = 12345678910;
 
     Console.WriteLine(tutar * 1.18);
@@ -27,3 +80,4 @@ public class Vatandas
     public int DogumYili { get; set; }
     public long TcNo { get; set; }
 }
+
