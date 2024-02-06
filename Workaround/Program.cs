ae5f69d48d0f8f09b7f26e0a1edc1896a8834f09
Asim Mutlu <asimmutlu6134@gmail.com>
2024-02-06 13:50:26 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿//Degiskenler();
+
+Vatandas vatandas = new Vatandas();
+
+Console.ReadLine();
+
+static void Degiskenler()
+{
+    string mesaj = "Merhaba";
+    double tutar = 100000; //db den gelir.
+    int sayi = 100;
+    bool girisYapmisMi = false;
+
+    string ad = "Engin";
+    string soyad = "Demiroğ";
+    int dogumYili = 1985;
+    long tcNo = 12345678910;
+
+    Console.WriteLine(tutar * 1.18);
+}
+
+public class Vatandas
+{
+    // Pascal casing
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+}
